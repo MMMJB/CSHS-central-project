c2c818ccb56b566336fce47b292a1f16439258ab
MMMJB <michaelbeck445@gmail.com>
2024-04-08 22:42:01 -0400

    its big now

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import * as handTrack from 'handtrackjs';
-import { flapBird } from './game';
+import * as handTrack from "handtrackjs";
+import { flapBird } from "./game";
 
 const video = document.querySelector("video#WEBCAM");
 const canvas = document.querySelector("canvas#WEBCAM");
@@ -11,36 +11,41 @@ const modelParams = {
   maxNumBoxes: 3, // maximum number of boxes to detect
   iouThreshold: 0.5, // ioU threshold for non-max suppression
   scoreThreshold: 0.6, // confidence threshold for predictions.
+  flipHorizontal: true,
 };
 
-let state = "open";
+window.handState = "open";
 
-video.addEventListener("loadeddata", async function() {
-    const start = Date.now();
-    const model =  await handTrack.load(modelParams);
-    console.log("Model loaded in: ", Date.now() - start, "ms");
+video.addEventListener("loadeddata", async function () {
+  const start = Date.now();
+  const model = await handTrack.load(modelParams);
+  console.log("Model loaded in: ", Date.now() - start, "ms");
 
-    function detectHand() {
-         model.detect(video).then((predictions) => {
-            model.renderPredictions(predictions, canvas, context, video);
-            
-            const hands = predictions.filter(prediction => prediction.label === "open" || prediction.label === "closed");
-            if (hands.length === 0) {
-              state = "open";
-              return requestAnimationFrame(detectHand);
-            }
-            const action = predictions[0].label;
+  function detectHand() {
+    model.detect(video).then((predictions) => {
+      model.renderPredictions(predictions, canvas, context, video);
 
-            if (state === "open" && action === "closed"){
-              flapBird()
-            }
-            
-            state = action;
+      const hands = predictions.filter((prediction) =>
+        ["open", "closed", "pinch"].includes(prediction.label)
+      );
 
-            requestAnimationFrame(detectHand);
-        });
-    }
+      if (hands.length === 0) {
+        window.handState = "open";
+        return requestAnimationFrame(detectHand);
+      }
 
-    detectHand();
-})
+      const recent = hands[0].label;
+      const action = ["open", "closed"].includes(recent) ? recent : "closed";
 
+      if (window.handState === "open" && action === "closed") {
+        flapBird();
+      }
+
+      window.handState = action;
+
+      requestAnimationFrame(detectHand);
+    });
+  }
+
+  detectHand();
+});
